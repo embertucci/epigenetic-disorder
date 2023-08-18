0830dbd7fce51cb50d608c5a010e887697282b3c
Emily Bertucci-Richter <31318190+embertucci@users.noreply.github.com>
2023-08-18 13:45:21 -0400

    Update calculate_PDN.R

--- calculate_PDN.R
+++ calculate_PDN.R
@@ -150,38 +150,40 @@ filePaths <- paste0("/scratch/emb19132/GENOME_PDR/RAT/sam_no_header/", unique_fi
 for(i in 1:length(unique_fileID)){
   ##read in SAM file written as a plain text file with headers removed
   SAM <- read.table(filePaths[i], header = FALSE, fill = TRUE)
-  ##remove XM:Z: string tag from methylation string (column V14)
+  ## remove XM:Z: string tag from methylation string (column V14)
   SAM$plain.string <- str_remove_all(SAM$V14, "XM:Z:")
-  ##count number of CpGs in string
+  ## count number of CpGs in string
   SAM$number.of.unmeth.cpg <- str_count(SAM$plain.string, "z")
   SAM$number.of.meth.cpg <- str_count(SAM$plain.string, "Z")
   SAM$number.of.cpg <- (SAM$number.of.meth.cpg + SAM$number.of.unmeth.cpg)
   SAM$actual.read.length <- str_count(SAM$plain.string, "") ##should be same as CIGAR specification if no mismatches
-  ##extract just the CpGs in the methylation string
+  ## extract just the CpGs in the methylation string
   SAM$cpg.meth.string <- gsub("[^zZ]", "", SAM$plain.string)
-  ##calculate number of neighbors (i.e. observations of pairs that could be concordant/discordant)
+  ## calculate number of neighbors (i.e. observations of pairs that could be concordant/discordant)
   SAM$num.neighbors <- (((SAM$number.of.cpg - 2)*2)+(2))
-  ##set negative values (to cytosines with no cytosines) to zero
+  ## set negative values (to cytosines with no cytosines) to zero
   SAM$num.neighbors[SAM$num.neighbors < 0] <- 0
-  ##count discordant occcurances of a string
+  ## count discordant occcurances of a string
   SAM$zZ.occur <- str_count(SAM$cpg.meth.string,"zZ")
   SAM$Zz.occur <- str_count(SAM$cpg.meth.string,"Zz")
-  ##for each occurance of zZ or Zz, read loses 2 concordance points from max concordance
+  ## for each occurance of zZ or Zz, read lose 2 concordance points from max concordance
   SAM$discordant.occur <- (SAM$zZ.occur + SAM$Zz.occur)
   SAM$discordance.deduction <- (SAM$discordant.occur*2)
   SAM$concordance.score <- (SAM$num.neighbors - SAM$discordance.deduction)
   SAM$concordance.perc <- (SAM$concordance.score/SAM$num.neighbors)
-  ##print only read information as a text file for each sample
+  SAM$read.discordance.perc <- (1 - SAM$concordance.perc) ##perc of discordance per read 
+  ## print only read inforation as a text file for each sample
   SAM$zero.based.start <- (SAM$V4 - 1)
   SAM$zero.based.end <- (SAM$zero.based.start + SAM$actual.read.length)
   SAM$POS <- SAM$V4
   SAM$CIGAR <- SAM$V6
   SAM$CHROM <- SAM$V3
-  cleaned.SAM <- na.omit(SAM) ##remove NAs
-  write.table(cleaned.SAM, file=paste0("/scratch/emb19132/GENOME_PDR/RAT/PDN_output/", unique_fileID[i], "_cleaned_SAM.txt"))
-  ##now make txt file in bed format for each with the 0 based start and end positions after removing cytosines without a neighbor on the nearest read (0 or 1 CpG)
-  perRead_bed <- cleaned.SAM[,c(34,30,31,1,29)]
-  #Write the result table in an individual text file
+  ## remove NAs so you can calculate average and remove any reads with 1 or fewer CpGs
+  cleaned.SAM <- na.omit(SAM)
+  ## write.table(cleaned.SAM, file=paste0(opt$outputDir,"/", opt$fileID, "_cleaned_SAM.txt"))
+  ## now make txt file in bed format for each with the 0 based start and end positions after removing cytosines without a neighbor on the nearest read (0 or 1 CpG)
+  perRead_bed <- cleaned.SAM[,c("CHROM","zero.based.start","zero.based.end","V1","read.discordance.perc")]
+  ## write the result table in an individual text file
   write.table(perRead_bed, file=paste0("/scratch/emb19132/GENOME_PDR/RAT/PDN_output/", unique_fileID[i], "_PDN_per_read.txt"))
 }
 
